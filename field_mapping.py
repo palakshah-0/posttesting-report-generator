ccd93e458a717c2742d2c42b7b64abff109a6628
palakshah-0 <palaksha@bu.edu>
2026-09-03 13:47:35 -0700

    Update field_mapping.py

--- field_mapping.py
+++ field_mapping.py
@@ -1,16 +1,24 @@
 POST_TEST_FIELD_MAP = {
+    # Record ID
     "sub_id_number": "sub_id_number",
 
+    # CTOPP-2
     "ctopp_elision_std_post": "ctopp_elision_std_post",
     "ctopp_nwr_std_post": "ctopp_nwr_std_post",
-    "dibels_words_correct_post": "dibels_words_correct_post",
 
+    # DIBELS
+    "dibels_words_correct_post": "dibels_words_correct_post",
+    "dibels_total_words_post": "dibels_total_words_post",
+
+    # WRMT-III
     "wrmt_wid_std_post": "wrmt_wid_std_post",
     "wrmt_wa_std_post": "wrmt_wa_std_post",
     "wrmt_pc_std_post": "wrmt_pc_std_post",
 
+    # TOWRE-2
     "towre_swe_std_post": "towre_swe_std_post",
     "towre_pde_std_post": "towre_pde_std_post",
 
+    # PPVT-5
     "ppvt_std_post": "ppvt_std_post",
 }
